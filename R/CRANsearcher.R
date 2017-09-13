8b3b3d2ecc8845b0b1a37553f6a44d3071399176
Mark Badros <mbadros@gmail.com>
2017-09-13 18:17:12 -0400

    Change flex display

--- R/CRANsearcher.R
+++ R/CRANsearcher.R
@@ -60,10 +60,10 @@ CRANsearcher <- function(){
       fillCol(
         flex=c(1,6),
         fillRow(
-          flex=c(3,2,1),
-          textInput("search","Enter search terms separated by commas (e.g. latent class, longitudinal)", width="90%"),
-          selectInput("dates","Last release date range",choices=c("1 month","3 months","6 months","12 months","All time"), selected="All time", width="80%"),
-          checkboxInput("installed", "Include only installed packages?", value = FALSE, width = "50%")),
+          flex=c(2,2,1),
+          textInput("search","Enter search terms separated by commas (e.g. latent class, longitudinal)", width="60%"),
+          selectInput("dates","Last release date range",choices=c("1 month","3 months","6 months","12 months","All time"), selected="All time", width="60%"),
+          checkboxInput("installed", "Include only installed packages?", value = FALSE, width = "60%")),
         div(DT::dataTableOutput("table"), style = "font-size: 90%")
       )
     ),
